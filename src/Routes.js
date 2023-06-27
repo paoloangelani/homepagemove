757122566602e9a74fb980dc46cfb2be09ef5145
Paolo Angelani <119342177+paoloangelani@users.noreply.github.com>
2023-06-27 18:20:44 +0200

    impostazione routes pages and layout, remember to put the avatar

---
+++ src/Routes.js
@@ -0,0 +1,5 @@
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import UnknowPage from "./pages/UnknowPage";
+
+export { HomePage, ContactPage, UnknowPage };
---
+++ src/pages/UnknowPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Unknow from "../components/layout/Unknow";
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Unknow />
+    </div>
+  );
+};
+
+export default ContactPage;
